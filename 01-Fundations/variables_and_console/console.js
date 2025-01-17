bfa678db3438ef6e576ea1bd0c70283c70b03d36
your_github_username <valentinaleonh8@gmail.com>
2025-01-16 23:57:57 -0600

    Start JS Course - Fundamentals

---
+++ 01-Fundations/variables_and_console/console.js
@@ -0,0 +1,11 @@
+let a = 10,
+    b = 20, 
+    c = 30,
+    d = 40,
+    x = a + b;
+
+console.log( x );
+console.warn( x );
+console.error( x );
+console.log({a}); // print the value of the variable
+console.log({x});
