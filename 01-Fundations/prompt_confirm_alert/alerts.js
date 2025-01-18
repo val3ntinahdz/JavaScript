fb2e20e549e89531aa6ceffcb1990b39f63226d5
your_github_username <valentinaleonh8@gmail.com>
2025-01-17 21:12:47 -0600

    Add confirm, alert and prompt examples. Explain Global Object in Node.js

---
+++ 01-Fundations/prompt_confirm_alert/alerts.js
@@ -0,0 +1,12 @@
+alert('Catastrophic blues, moving on was always easy for me to do');
+let input = prompt('Oh! my love is lie...'); // Ask the user for information
+console.log('*****' + input + '*******');
+
+
+const selection = confirm("If you don't press 'Accept', then you are a Kanye West's fan");
+console.log(selection)
+
+Alert, Prompt and Confirm are 'window' objects. 
+
+// The Global Object in node.js is a built-in object that provides global variables and functions accessible throughout the Node.js runtime environment. It is analogous to the window object in browsers but tailored for server-side JavaScript. It is analogous to the Window object in browsers but tailored for server-side JS.
+console.log(global)
