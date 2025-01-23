ee0f677c645b98627fa2c7bc418bf8a6348a6bda
your_github_username <valentinaleonh8@gmail.com>
2025-01-22 23:33:17 -0600

    Add 'arrays' subject

---
+++ subjects/arrays/arrays.js
@@ -0,0 +1,23 @@
+// const arr = new Array(10);
+// console.log(arr)
+
+let tvSeries = ['Good Omens', 'Fleabag', 'HIMYM', 'Anne with an E'];
+console.log(tvSeries)
+
+console.log(tvSeries[3]); // Access the third position in the array
+
+let arr = [
+    true,
+    123, 
+    'Cool',
+    1 + 2,
+    Math.sqrt(16),
+    function(){},
+    ()=>{},
+    ['Gilbert', 'Anne', 'Maryla', 'Diane', 'Jerry', [
+        'Mathew', 'Rachel'
+    ]]
+];
+
+console.log(arr[7][4]);
+console.log(arr[7][5][0]);
