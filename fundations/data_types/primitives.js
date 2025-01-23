f0c5f00576d73d4070615b5e15308bfa94603f3a
your_github_username <valentinaleonh8@gmail.com>
2025-01-22 22:30:51 -0600

    Write primitive data_types

---
+++ fundations/data_types/primitives.js
@@ -0,0 +1,29 @@
+// String
+artist = "Taylor Swift";
+console.log(typeof artist);
+
+// Number
+let speed_light = 299.792458; 
+console.log(typeof speed_light);
+
+let my_height = 1.69;
+console.log(typeof my_height);
+
+// Boolean 
+let goodMusic = true;
+console.log(typeof goodMusic);
+
+// Undefined 
+let favoriteBooks;
+console.log(typeof favoriteBooks);
+
+// Null
+let imNull = null;
+console.log(typeof imNull);
+
+// Symbol 
+let symbol1 = Symbol('a');
+let symbol2 = Symbol('a');
+console.log(typeof symbol1);
+
+console.log(symbol1 === symbol2);
