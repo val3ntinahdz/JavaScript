cbf46c4d2e6f0433d7d620494e4a3a6abb4f8421
your_github_username <valentinaleonh8@gmail.com>
2025-02-05 22:55:02 -0600

    Work with literal objects and its properties

--- subjects/literal_objects/objects.js
+++ subjects/literal_objects/objects.js
@@ -1,7 +1,8 @@
 // objects are like python dictionaries
 // objects have key-vale pairs in their structure
 // uses lowerCamelCase with property names
-let character = {
+
+const character = {
     name: 'Tony Stark',
     codeName: 'Ironman',
     alive: false, 
@@ -33,4 +34,34 @@ console.log('New codename:', character.codeName[character.codeName.length - 1]);
 const x = 'alive';
 console.log('alive?', character[x]);
 
+// Remove a property
+delete character.age;
+console.log(character);
 
+// Create a new key-value pair
+character.married = true;
+
+// Convert the object into an array
+const entriesPairs = Object.entries(character);
+console.log(entriesPairs);
+
+console.log(character);
+// character = true;
+
+// Freezes the direct assignations
+Object.freeze(character);
+
+character.cash = 10000000000000;
+character.married = false;
+character.address.location = 'Bali';
+console.log(character);
+
+// Extract the Object's properties
+const properties = Object.getOwnPropertyNames(character);
+console.log("Properties:", properties);
+
+// Get the Object's values
+const val = Object.values(character);
+console.log("Values:", val);
+
+// Check the documenation for reference => https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Object
