69bbe81d75a1ce1ded65612c7652761371d44969
your_github_username <valentinaleonh8@gmail.com>
2025-02-05 20:40:27 -0600

    Complete 'arrays' lecture

--- subjects/arrays/arrays2.js
+++ subjects/arrays/arrays2.js
@@ -9,3 +9,25 @@ console.log({first, last});
 artists.forEach((element, index, arr) => {
     console.log({element, index, arr});
 });
+
+// .push() appends new elements to an array, and returns the new length of the array.
+newElement = artists.push('Michael Jackson');
+console.log({newElement, artists});
+
+// .unshift() inserts new elements at the start of the array
+firstArtist = artists.unshift('Frank Sinatra');
+console.log({firstArtist, artists});
+
+// pop() removes the last inserted element in the array
+let removedArtist = artists.pop();
+console.log({removedArtist, artists});
+
+// .splice() removes elements from an array and, if necessary, inserts new elements in their place, returning the deleted elements.
+let position = 1;
+
+let removedArtists = artists.splice(position, 2);
+console.log(removedArtists, artists);
+
+// .indexOf() returns the position of a specified element
+let dpIndex = artists.indexOf('Daft Punk');
+console.log({dpIndex});
