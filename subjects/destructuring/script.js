001575ac9ee9d60c0d86c86f0e461fb835e28a38
your_github_username <valentinaleonh8@gmail.com>
2025-02-11 19:38:25 -0600

    Pro tips: functions and passing arguments

---
+++ subjects/destructuring/script.js
@@ -0,0 +1,35 @@
+'use strict';
+
+// function createPerson( firstName, lastName ) {
+//     return {
+//         firstName,
+//         lastName
+//     }
+// }
+
+const createPerson = ( firstName, lastName ) => ({ firstName, lastName });
+
+const person = createPerson( 'Ford', 'Prefect' );
+console.log( person );
+
+function printArguments() {
+    console.log( arguments );
+}
+
+
+// The rest parameter syntax allows a function to accept an indefinite number of arguments as an array
+// Check => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/rest_par for reference
+const printArguments2 = (...args) => {
+    return args;
+}
+
+printArguments(42, true, false, "rouge", "La vie est belle");
+
+// const arrayArgs = printArguments2(42, true, false, "Beeblebrox", "...est belle");
+// console.log( arrayArgs );
+
+const [ number, logical, mentallyStable, mrPresident, laVie ] = printArguments2(42, true, false, "Zaphod Beeblebrox", "...est belle");
+console.log({ number, logical, mentallyStable, mrPresident, laVie });
+
+const { lastName: newLastName } = createPerson('Valentina', 'León' );
+console.log({ newLastName });
