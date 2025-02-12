09fb312973bdca66712951f96f1ade9dab7230d5
your_github_username <valentinaleonh8@gmail.com>
2025-02-11 18:13:51 -0600

    Explain 'return functions' topic

--- subjects/functions/functions.js
+++ subjects/functions/functions.js
@@ -2,6 +2,10 @@
 function greet(name) {
     console.log(arguments); // Built-in object to send the functions multiple arguments
     console.log("Hey! " + name); // An operator is used to concatenate elements
+    return [1, 3, 5556, 9]; // No code executes after 'return'
+
+    // This code will never execute!
+    console.log("i am a code after return");
 }
 
 // Anonimous function
@@ -18,6 +22,10 @@ const greetArrow2 = (name) => {
     console.log("This is an arrow! " + name);
 }
 
-greet('Valentina', 18, 'red');
-greet2('Valentina');
-greetArrow2('Zoé');
+
+const returnGreet = greet('Valentina', 18, true, 'Mexico');
+console.log(returnGreet);
+
+// greet('Valentina', 18, 'red');
+// greet2('Valentina');
+// greetArrow2('Zoé');
