63df1f2f8ca39b9fb6a6427cf824c51e3d1cba20
your_github_username <valentinaleonh8@gmail.com>
2025-02-21 22:31:00 -0600

    Attach a function to ask and choose a card randomly from the deck

--- js_projects/blackjack/assets/js/game.js
+++ js_projects/blackjack/assets/js/game.js
@@ -9,6 +9,8 @@ let deck = [];
 const types = ['C', 'D', 'H', 'S'];
 const extraCards = ['A', 'J', 'Q', 'K'];
 
+
+// This function creates a new deck
 const createDeck = () => {
     for(let i = 2; i <= 10; i++) {
         for(let type of types) {
@@ -22,11 +24,28 @@ const createDeck = () => {
         }
     }
 
-    console.log(deck);
+    // console.log(deck);
     deck = _.shuffle(deck);
     console.log(deck);
     return deck;
 }
 
-
 createDeck();
+
+// This function allows the user to ask for a card
+const askForCard = () => {
+    if (deck.length === 0) {
+        console.log('Deck is empty!');
+        return null;
+    }
+    
+    const i = Math.floor(Math.random() * deck.length);
+    const randomCard = deck[i];
+    deck.splice(i, 1);
+
+    console.log('Random card:', randomCard);
+    console.log(deck);
+    return randomCard; // return the drawn card
+}
+
+askForCard();
