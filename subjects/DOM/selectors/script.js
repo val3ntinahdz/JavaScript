07cb907cd6c31062002b2e6d6c50e6de8e350fb7
your_github_username <valentinaleonh8@gmail.com>
2025-02-23 18:58:43 -0600

    Introduction to DOM: manipulating and creating elements

--- subjects/DOM/selectors/script.js
+++ subjects/DOM/selectors/script.js
@@ -7,3 +7,14 @@ console.log(paragraph);
 const paragraphs = document.querySelectorAll('p');
 console.log(paragraphs);
 console.log(paragraphs[0].nodeName); 
+
+// import the function into the DOM with .onload
+document.body.onload = addElement; 
+
+function addElement() {
+    const content = document.querySelector('.content');
+    const newHeader = document.createElement('h3');
+    newHeader.innerText = 'Fundamental data types in the DOM';
+
+    content.appendChild(newHeader);
+}
