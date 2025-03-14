a1ca8d4f80c77b37d12191440c427cd48217502c
your_github_username <valentinaleonh8@gmail.com>
2025-03-13 20:00:05 -0600

    feat: add event listener to toggle button to change background color

--- subjects/DOM/styles-and-classes/theme-switcher/script.js
+++ subjects/DOM/styles-and-classes/theme-switcher/script.js
@@ -5,7 +5,25 @@ console.log(themeSelector);
 function changeBackgroundColor(event) {
     document.body.classList.remove("default", "blue-theme", "green-theme", "red-theme");
     document.body.classList.add(event.target.value);
-    localStorage.setItem("theme", event.target.value);
+    localStorage.setItem("theme", event.target.value); // https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
+    console.log(themeSelector.value);
 }
 
 themeSelector.addEventListener("change", changeBackgroundColor);
+
+document.body.addEventListener("DOMContentLoaded", () => {
+    const savedTheme = localStorage.getItem("theme") || "default";
+    document.body.classList.add(savedTheme);
+    themeSelector.value = savedTheme;
+});
+
+
+const toggleButton = document.getElementById('toggle-theme');
+
+function toggleBackgroundColor() {
+    document.body.classList.remove("toggle-theme");
+    document.body.classList.add("dark-mode");
+    localStorage.setItem("theme", "dark-mode");
+}
+
+toggleButton.addEventListener("click", toggleBackgroundColor);
