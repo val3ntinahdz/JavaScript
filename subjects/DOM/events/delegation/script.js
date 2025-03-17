261fc607e00d98e44809169ec5ef609e761593d9
your_github_username <valentinaleonh8@gmail.com>
2025-03-16 22:08:14 -0600

    Complete first event delegation exercise: toggle list

--- subjects/DOM/events/delegation/script.js
+++ subjects/DOM/events/delegation/script.js
@@ -1,13 +1,14 @@
-// const tree = document.getElementById('tree');
+// Select the common ancestor of all <li> elements
 const tree = document.querySelector('.tree');
 console.log(tree);
 
+// Attach an event listener to the tree
 tree.addEventListener('click', function(e) {
-    const target = e.target;
-    console.log(target);
-    console.log(target.tagName);
-    if(target.tagName != 'LI') return;
-    for(let li of target.children) {
-        li.hidden = !li.hidden;
+    const target = e.target; // save the event target (the current selected elem)
+    // console.log(target);
+    // console.log(target.tagName);
+    if(target.tagName != 'LI') return; // if an element outside the LI is clicked, do nothing
+    for(const li of target.children) {
+        li.hidden = !li.hidden; // toggle 'hidden' attribute in each <li>
     }
 });
