e2ee67e2fac057b99f9ca5bb9e18d83aaeb5268b
Valentina Leon <valentinaleonh8@gmail.com>
2025-05-04 15:43:01 -0600

    Refactoring code into modules

---
+++ projects/blackjack-vite/src/blackjack/usecases/create-deck.js
@@ -0,0 +1,21 @@
+import _ from 'underscore';
+
+// the 
+export const crearDeck = (tiposDeCarta, tiposEspeciales) => {
+
+    for( let i = 2; i <= 10; i++ ) {
+        for( let tipo of tiposDeCarta ) {
+            deck.push( i + tipo);
+        }
+    }
+
+    for( let tipo of tiposDeCarta ) {
+        for( let esp of tiposEspeciales ) {
+            deck.push( esp + tipo);
+        }
+    }
+    // console.log( deck );
+    deck = _.shuffle( deck );
+    console.log( deck );
+    return deck;
+}
