b8c17eadd31b01d6366b4bd7ffc7e7a020d66394
your_github_username <valentinaleonh8@gmail.com>
2025-05-28 17:54:28 -0600

    Callbacks: handle errors

--- subjects/advanced-js/src/concepts/callbacks.js
+++ subjects/advanced-js/src/concepts/callbacks.js
@@ -6,8 +6,15 @@ import { heroes } from "../data/heroes";
  */
 export const callbacksComponent = (element) => {
 	const id = "5d86371f2343e37870b91ef12";
-	findHero(id, (hero) => { // destructuring name
-		element.innerHTML = hero?.name || "There's no hero";  // Hulk
+	findHero(id, (error, hero) => { // destructuring name
+		// element.innerHTML = hero?.name || "There's no hero";
+
+		if (error) {
+			element.innerHTML = error;
+			return; // this way we can handle error
+		}
+
+		element.innerHTML = hero.name; // Hulk
 	});
 	
 }
@@ -15,10 +22,16 @@ export const callbacksComponent = (element) => {
 /**
  * 
  * @param {String} id 
- * @param {(hero: Object)=> void} callback 
+ * @param {(error?: String, hero: Object)=> void} callback 
  */
 const findHero = (id, callback) => {
 	const hero = heroes.find(hero => hero.id === id);
 
-	callback(hero); // this is a function!
+	if (!hero) {
+		callback(`Hero with id ${id} not found.`);
+		return; // undefined
+	}
+
+	// we pass an error and an object (in case hero exists, it is return, otherwised UNDEFINED)
+	callback(null, hero); // this is a function!
 }
