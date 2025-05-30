920a5dfb0e2ec3617ea794ce3bd835309892c24f
your_github_username <valentinaleonh8@gmail.com>
2025-05-29 22:52:49 -0600

    Practice callbacks: callbacks and callback hell

--- subjects/asynchronous-javascript/callbacks/script.js
+++ subjects/asynchronous-javascript/callbacks/script.js
@@ -1,21 +1,49 @@
-import { heroes } from "./data/heroes"
+let stocks = {
+    Fruits: ["strawberry", "grapes", "banana", "apple"],
+    liquid: ["water", "ice"],
+    holder: ["cone", "cup", "stick"],
+    toppings: ["chocolate", "peanuts"],
+};
 
-export const callbacksComponent = (element) => {\
-    const id = "5d86371f233c9f2425f16916"
-    findHero(id, (blackWidow) =>{
-        element.innerHTML = blackWidow.name;
-    });
+let order = (fruit_name, call_production) => {
 
+    setTimeout(() => {
+        console.log(`${stocks.Fruits[fruit_name]} was selected`);
+        call_production();
+
+    }, 2000)
 }
 
-/**
- * 
- * @param {String} id 
- * @param {(hero:Object) => void} callback 
- */
-// A callback is a function received as an argument.
-const findHero = (id, callback) => {
-    const hero = heroes.find(hero => hero.id === id);
 
-    callback(hero);
+// callback hell
+let production = () => {
+    setTimeout(() => {
+        console.log("production has started")
+
+        setTimeout(() => {
+            console.log("the food has been chopped");
+
+            setTimeout(() => {
+                console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was added`)
+
+                setTimeout(() => {
+                    console.log("the machine was started");
+
+                    setTimeout(() => {
+                        console.log(`ice cream was placed on a ${stocks.holder[0]}`);
+
+                        setTimeout(() => {
+                            console.log(`${stocks.toppings[1]} was added`);
+
+                            setTimeout(() => {
+                                console.log("serve ice cream!");
+                            }, 2000)
+                        }, 3000)
+                    }, 2000)
+                }, 1000)
+            }, 1000)
+        }, 2000)
+    }, 0o0)
 }
+
+order(2, production);
