78e0bc62d4ea76470e19d116638402278101dbb6
your_github_username <valentinaleonh8@gmail.com>
2025-05-30 17:22:38 -0600

    Work with promises & handle errors

--- subjects/advanced-js/src/concepts/promises.js
+++ subjects/advanced-js/src/concepts/promises.js
@@ -5,6 +5,22 @@ import { heroes } from "../data/heroes";
  * @param {HTMLDivElement} element 
  */
 export const promiseComponent = (element) => {
+    const renderHero = (hero) => {
+        element.innerHTML = hero.name;
+    }
+
+    const renderError = (error) => {
+        element.innerHTML = `
+        <h1>Error:</h1>
+        <h3>${error}</h3>
+        `
+    }
+
+    const id1 = '5d86371f1efebc31def272e2';
+
+    findHero(id1)
+        .then(renderHero)
+        .catch(renderError)
    
 }
 
@@ -20,7 +36,7 @@ const findHero = (id) => {
         const hero = heroes.find(hero => hero.id === id);
 
         if (hero) {
-            resolve(hero);
+            result(hero);
             return;
         }
 
