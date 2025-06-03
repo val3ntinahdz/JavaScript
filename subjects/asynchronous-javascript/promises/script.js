e84e79bd0b1871c786d9ad3a0038ef6d5db5d989
your_github_username <valentinaleonh8@gmail.com>
2025-06-02 20:55:48 -0600

    Approach promise exercise

--- subjects/asynchronous-javascript/promises/script.js
+++ subjects/asynchronous-javascript/promises/script.js
@@ -1,5 +1,26 @@
-function delay(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+// function delay(ms) {
+//     return new Promise(resolve => setTimeout(resolve, ms));
+// }
 
-delay(3000).then(() => alert('runs after 3 seconds'));
+// delay(3000).then(() => alert('runs after 3 seconds'));
+
+
+// show circle with callback vs promises
+// showCircle(cx, cy, radius, callback) = () => {
+//     callback(calculateRadius);
+    
+// }
+
+ 
+const calculateRadius = () => {
+    console.log("calculate radius function connnected");
+
+    const div = document.createElement('div');
+    div.style.width = 150;
+    div.style.height = 150;
+    div.style.backgroundColor = "red";
+    div.className = 'circle';
+
+    document.body.append(div);
+    const computedStyle = getComputedStyle(div);
+}
