7b702c314fc5de772f4ab467331a7926ab9ae4ce
your_github_username <valentinaleonh8@gmail.com>
2025-06-05 22:33:29 -0600

    Add promise exception handling with async

--- subjects/advanced-js/src/concepts/05-async.js
+++ subjects/advanced-js/src/concepts/05-async.js
@@ -1,13 +1,26 @@
 import { heroes } from "../data/heroes";
 
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
 export const asyncComponent = (element) => {
-    const id1 = "5d86371fd55e2e2a30fe1ccb2";
+    const id1 = "5d86371fd55e2e2a30fe1ccb9";
 
     findHero(id1)
         .then( name => element.innerHTML = name)
+        .catch( error => element.innerHTML = error)
 }
 
+/**
+ * 
+ * @param {String} id 
+ * @returns {Promise<String>}
+ */
 const findHero = async( id ) => {
     const hero = heroes.find(hero => hero.id === id);
-    return hero.name;
+    if (!hero)
+        throw `hero with id ${id} not found`;
+
+    return hero.name; // in an async function, a "return" means everything's going well
 }
