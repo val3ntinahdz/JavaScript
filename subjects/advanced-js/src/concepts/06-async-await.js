1414bf51b5341a330d6ecdeee50f7c54adb9835e
your_github_username <valentinaleonh8@gmail.com>
2025-06-06 12:55:25 -0600

    Exception handling with async/await promises

--- subjects/advanced-js/src/concepts/06-async-await.js
+++ subjects/advanced-js/src/concepts/06-async-await.js
@@ -8,13 +8,16 @@ export const asyncAwaitComponent = async(element) => {
     const id1 = "5d86371f25a058e5b1c8a65e";
     const id2  = "5d86371f1efebc31def272e2";
 
-    // value destructuring
-
-    // wait for findHero (it's a promise) to resolve, then extract the 'name' property from the result and store it in 'name1'
-    const { name: name1 } = await findHero(id1);
-    const { name: name2 } = await findHero(id2);
-
-    element.innerHTML = `${ name1 } & ${ name2 }`;
+    try {
+        // wait for findHero (it's a promise) to resolve, then extract the 'name' property from the result and store it in 'name1'
+        const { name: name1 } = await findHero(id1);
+        const { name: name2 } = await findHero(id2);
+    
+        element.innerHTML = `${ name1 } & ${ name2 }`;
+        
+    } catch (error) {
+        element.innerHTML = error;
+    }
 
 }
 
