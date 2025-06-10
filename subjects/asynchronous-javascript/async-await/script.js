7909be1c13b90feb7fb17b7c2d3b1ed96bd8fd77
your_github_username <valentinaleonh8@gmail.com>
2025-06-09 22:26:44 -0600

    Fetch jokes API using async/await

---
+++ subjects/asynchronous-javascript/async-await/script.js
@@ -0,0 +1,34 @@
+// Examples with ASYNC/AWAIT
+// Async is a keyword used to declare asynchronous functions in JavaScript. 
+// In other words, the functions with the word 'async' before will always return a promise
+
+// Turn function into async/await syntax
+
+import axiosRequest from 'https://esm.sh/axios';
+
+async function parseJokes() {
+
+    const result = await axiosRequest.get("https://v2.jokeapi.dev/joke/Any")
+    const { joke, type, setup, delivery } = result.data;
+
+    if (type === "single") {
+        if (joke.toLowerCase().startsWith("c")) {
+            return joke;
+        }
+
+    } else if (type === "twopart") {
+
+        if (
+            setup.toLowerCase().startsWith('c') && 
+            (delivery.toLowerCase().startsWith('c'))
+        ) {
+            console.log(`This jokes start with letter C > ${setup} - ${delivery}`);
+        }
+    }
+
+
+    const theActualJoke = joke ?? `${setup} - ${delivery}`;
+    console.log("The actual joke: ", theActualJoke);
+    
+    return theActualJoke;
+}
