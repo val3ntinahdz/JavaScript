527bc761be49061485fc0928c85051f3abe561d5
val3ntinahdz <valentinaleonh8@gmail.com>
2025-06-22 22:39:46 -0600

    Further improvements: handle Bigint objects and enhance readability

--- subjects/asynchrony/generators/exercises/LCG-sequence/script.js
+++ subjects/asynchrony/generators/exercises/LCG-sequence/script.js
@@ -1,28 +1,27 @@
 // This exercise is from: https://javascript.info/generators
-function* pseudoRandomGenerator(seed) {
+
+function* pseudoRandomGenerator(seed, multiplier = 16807n, modulus = 2147483647n, maxLimit = Infinity) {
+    
     // throw an error if the seed does not exist
-    if (seed === 0 || !seed) 
-        throw new Error("Not a valid seed. Try again");
+    if (typeof seed !== 'bigint' || seed <= 0n) 
+        throw new Error("Seed must be a positive BigInt");
         
+    // limit iterations
+    let count = 0;
+
     // keep score of the current generated value
     let curr = seed;
     yield curr;
-    const seedExist = true;
 
-    do {
-        curr = curr * 16807 % 2147483647; // generate the next value;
+    while (++count < maxLimit) {
+        curr = (curr * multiplier) % modulus;
         yield curr;
-    } while (seedExist)
-
-
-    seedExist = false;
+    }
 }
 
-const randGenerator = pseudoRandomGenerator(42);
-console.log("Seed:", randGenerator.next().value); // 42
-console.log(randGenerator.next().value); // 705894
-console.log(randGenerator.next().value); // 1126542223
-console.log(randGenerator.next().value); // 1579310009
-console.log(randGenerator.next().value); // 565444343
-console.log(randGenerator.next().value); // 807934826
-console.log(randGenerator.next().value); // 421520601
+const randGenerator = pseudoRandomGenerator(427438753475347826823648726722n, 393403n, 2392839283n, 4);
+console.log("Seed:", randGenerator.next().value);
+console.log(randGenerator.next().value); 
+console.log(randGenerator.next().value);
+console.log(randGenerator.next().value);
+console.log(randGenerator.next().value);
