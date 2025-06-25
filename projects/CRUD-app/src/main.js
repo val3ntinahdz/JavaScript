16aa43084c78f78ff8629545047acb0b976794d4
val3ntinahdz <valentinaleonh8@gmail.com>
2025-06-24 23:03:57 -0600

    Load users - not implemented

--- projects/CRUD-app/src/main.js
+++ projects/CRUD-app/src/main.js
@@ -1,6 +1,7 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
+import { UsersApp } from './users/users-app'
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -11,13 +12,9 @@ document.querySelector('#app').innerHTML = `
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
     <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+    <div class="card"></div>
   </div>
 `
+const element = document.querySelector(".card");
+UsersApp(element);
 
-setupCounter(document.querySelector('#counter'))
